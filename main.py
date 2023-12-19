7bc599cbec426fb6d8b2fde7cc8fcadd396e5dd8
Matt <matthijswestra@hotmail.com>
2023-12-19 16:32:20 +0100

    Added initial work

--- main.py
+++ main.py
@@ -1,60 +1,155 @@
 # Neighbour joining with profiles
 # Nearest neighbour interchange
 # Top hits heuristic
+import random
 
 # Typehints
 profile = list[dict[str, float]]
 
-# Globals
-ALPHABET = 'ACGT'
-DATA_FILE = 'test-small.alm'
-L = -1                   # Will be set dynamically on loading the data
-
 class Node:
-    def __init__(self, nodeId: int):
+    def __init__(self, nodeId: int, parent: 'Node' = None, profile: profile = None):
         self.nodeId = nodeId
         self.children = []
-        self.parent = None
+        self.parent = parent
+        self.profile = profile
+        self.active = True
+        self.topHits = []
 
+    def findActiveAncestor(self) -> 'Node':
+        currentNode = self
+        while not currentNode.active:
+            currentNode = currentNode.parent
+        return currentNode
+
+
+# Globals
+ALPHABET = 'ACGT'
+DATA_FILE = 'test-small.aln'
+JOIN_ITERATIONS = 200
+JOINS_DONE = 0
+GENOME_LENGTH = 0
+MAX_ID = 0
+m = 0
+ACTIVE_NODES = 0
+
+# ======================= Util functions ====================================
 def readFile(fileName: str) -> list[str]:
+    """
+    Reads the file with the input data and parses it
+    :param fileName: The name of the file to where the input data resides
+    :return: the data as a list of all sequences
+    """
     with open(fileName, 'r') as file:
         rawData = file.readlines()
 
-    return [line for line in rawData if line[0] != '>']
+    return [line.strip() for line in rawData if line[0] != '>']
 
-def init():
-    data = readFile('')
+def createSeed(alphabet: str = ALPHABET, length: int = GENOME_LENGTH) -> str:
+    return ''.join(random.choice(alphabet) for _ in range(length))
 
-def initializeProfile(length: int) -> profile:
-    '''
-    Create a profile matrix of specified length initialized with all zeros.
-    This function only covers the 4 base pairs ACGT
-    '''
-    return [{base: 0 for base in ALPHABET} for _ in range(length)]
 
-def profileDistance(profile1: profile, profile2: profile):
-    '''
-    The profile distance of the new Node formed by joined profile 1 and profile 2 (A and B)
-    '''
+# ======================= Algorithm functions ================================
+def initializeProfile(genome: str = '') -> profile:
+    if genome == '':
+        return [{base: 0 for base in ALPHABET} for _ in range(GENOME_LENGTH)]
 
+    return [{base: float(genome[i] == base) for base in ALPHABET} for i in range(GENOME_LENGTH)]
+
+def computeTotalProfile() -> profile:
+    nodesToCheck = [ROOT_NODE]
+    newProfile = initializeProfile()
+    global ACTIVE_NODES
+    ACTIVE_NODES = -1
+
+    while nodesToCheck:
+        currentNode = nodesToCheck.pop(0)
+        for i in range(GENOME_LENGTH):
+            for key in currentNode.profile[i]:
+                newProfile[i][key] += currentNode.profile[i][key]
+
+        for child in currentNode.children:
+            if child.active:
+                nodesToCheck.append(child)
+        ACTIVE_NODES += 1
+
+    for i in range(GENOME_LENGTH):
+        for key in newProfile[i]:
+            newProfile[i][key] = newProfile[i][key] / ACTIVE_NODES
+
+    return newProfile
+
+def mergeProfiles(profile1: profile, profile2: profile):
+    return [{base: (profile1[i][base] + profile2[i][base]) / 2 for base in ALPHABET} for i in range(GENOME_LENGTH)]
+
+def profileDistance(i: profile, j: profile) -> float:
+    total = 0
+    # Go over every position
+    for l in range(GENOME_LENGTH):
+        # Calculate the distance between all possible combinations
+        for key in i[l].keys():
+            for otherKey in j[l].keys():
+                total += i[l][key] * j[l][otherKey] * int(key != otherKey)
+
+    return total / GENOME_LENGTH
+
+def nodeDistance(node1: Node, node2: Node) -> float:
+    return profileDistance(node1.profile, node2.profile) - upDistance(node1) - upDistance(node2)
+
+def upDistance(node: Node) -> float:
+    # Leafs are by definition 0
+    if not node.children:
+        return 0
+
+    return profileDistance(node.children[0].profile, node.children[1].profile) / 2
+
+def mergeNodes(node1: Node, node2: Node):
+    global MAX_ID
+    MAX_ID += 1
+    newNode = Node(MAX_ID, ROOT_NODE, mergeProfiles(node1.profile, node2.profile))
+    ROOT_NODE.children.remove(node1)
+    ROOT_NODE.children.remove(node2)
+    ROOT_NODE.children.append(newNode)
+    newNode.children = [node1, node2]
+    node1.active = False
+    node2.active = False
+    global JOINS_DONE
+    JOINS_DONE += 1
+
+    if JOINS_DONE >= JOIN_ITERATIONS:
+        global TOTAL_PROFILE
+        TOTAL_PROFILE = computeTotalProfile()
+
+def outDistance(node: Node) -> float:
+    if node.children:
+        return ACTIVE_NODES * profileDistance(node.profile, TOTAL_PROFILE) - profileDistance(node.children[0].profile, node.children[1].profile)
+
+    return ACTIVE_NODES * profileDistance(node.profile, TOTAL_PROFILE)
+
+def topHits(node: Node):
     pass
 
 
-def internalNodeDistance(node1, node2):
-    '''
-    The distance between two internal nodes
-    '''
+# =============================== Algorithm =======================================
+data = readFile(DATA_FILE)
+GENOME_LENGTH = len(data[0])
 
-    pass
+# Create initial topology
+ROOT_NODE = Node(-1, None, initializeProfile(''))
 
-def upDistance(node):
-    '''
-    Average distance of the node from it's children. 0 for leaf nodes
-    '''
-    pass
+for genome in data:
+    ROOT_NODE.children.append(Node(MAX_ID, ROOT_NODE, initializeProfile(genome)))
+    MAX_ID += 1
 
-def neighBourJoiningCriterion(i, j):
-    pass
+# Create total profile
+TOTAL_PROFILE = computeTotalProfile()
 
-def averageOutDistance(i):
-    pass
+# Find the top hits for each sequence
+m = len(data) ** 0.5
+seed = createSeed(ALPHABET, GENOME_LENGTH)
+results = []
+
+for child in ROOT_NODE.children:
+    results.append(outDistance(child))
+
+# TODO work it out
+
